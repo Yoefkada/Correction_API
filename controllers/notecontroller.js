852ae8ae3e700a089ccac111562b37c5a2d69608
Yoefkada <karadayoussef10@gmail.com>
2024-12-05 11:59:09 +0100

    correction

--- controllers/notecontroller.js
+++ controllers/notecontroller.js
@@ -1,16 +1,76 @@
-const prisma = require('../util/prismaclients');
+const prisma = require('../utils/prismaclients');
+
 
 async function addTags(req, res) {
     try {
         const data = req.body;
-        const note = await prisma.tag.create({data});
-        res.status(200).json({message : 'Tags added successfully !'})
-
+        const tag = await prisma.tag.create({data});
+        res.status(200).json({message : 'Tags added successfully'})
+        
     } catch (error) {
-        res.status(500).json({message : 'A problem has occured while adding tags'})
+        res.status(500).json({error: error.message})
     }
 }
 
+async function getTags (req, res){
+    try {
+        const tags =  await prisma.tag.findMany();
+        res.status(200).json(tags)
+    } catch (error) {
+        res.status(500).json({message: 'Internal server error'})
+    }
+}
+
+async function getTagById (req, res){
+    try {
+        const id = req.params.id;
+        const tag = await prisma.tag.findUnique({where: {id: id}});
+        if(tag){
+            res.status(200).json(tag)
+        }else{
+            res.status(404).json({message: 'Tag not found'})
+        }
+    } catch (error) {
+        res.status(500).json({message: 'Internal server error'})
+    }
+}
+
+async function updateTag(req, res){
+    try {
+        const {id} = req.params;
+        const data = req.body;
+        const tag = await prisma.tag.findUnique({where: {id: id}});
+        if(!tag)
+            return res.status(404).json({message: 'tag not found'})
+
+        const updatetag = await prisma.tag.update(
+            {where: {id: id}, data}
+        );
+        res.status(200).json(updatetag);
+    } catch (error) {
+        res.status(500).json({message: 'Internal server error'})
+    }
+}
+
+async function deleteTag(req, res){
+    try {
+        const {tagId} = req.params;
+        const tag = await prisma.tag.findUnique({where: {id: tagId}});
+        if(!tag)
+            return res.status(404).json({message: 'Tag not found'})
+        const deletedTag = await prisma.tag.delete({where: {id: tagId}});
+        
+        res.status(200).json(deletedTag);
+    } catch (error) {
+        res.status(500).json({message: 'Internal server error'})
+    }
+}
+
+
 module.exports = {
-    addTags
+    addTags,
+    getTags,
+    getTagById,
+    updateTag,
+    deleteTag,
 }
--- routes/noteroutes.js
+++ routes/noteroutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { addTags } = require('../controllers/notecontrollers');
+const { addTags, getTags, getTagById, updateTag, deleteTag } = require('../controllers/notecontrollers');
 const { validation, handleValidationErrors } = require('../utils/validator');
 
 
 router.post('/tags', validation.tagValidation, handleValidationErrors ,addTags);
+router.get('/tags', getTags);
+router.get('/tags/:id', getTagById);
+router.put('/tags/:id', validation.tagValidation, handleValidationErrors, updateTag);
+router.delete('/tags/:tagId', deleteTag);
+
+
 
 module.exports = router;
